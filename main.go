65c804f07c1568d1f3635abc1eb678d0faadf286
ss1119 <y.sasaki111919@gmail.com>
2021-12-01 16:23:04 +0900

    http/2サーバーを追加

--- main.go
+++ main.go
@@ -29,8 +29,19 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// 証明書
 	var crtPath = "../live/localhost111919.ml/fullchain.pem"
 	var keyPath = "../live/localhost111919.ml/privkey.pem"
+
+	// HTTP/3サーバー起動
 	handler := setupHandler()
 	http3.ListenAndServe(":8080", crtPath, keyPath, handler)
+
+	// HTTP/2サーバー起動
+	// http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
+	// http.HandleFunc("/", IndexHandler)
+	// err := http.ListenAndServeTLS(":8080", crtPath, keyPath, nil)
+	// if err != nil {
+	// 	log.Fatal("ListenAndServe: ", err)
+	// }
 }
